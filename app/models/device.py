e5c073c526f0c8af662de81c0e7170642910af00
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-27 10:59:48 +0300

    feat: finalize Device model with relationships and ForeignKey to Location

--- app/models/device.py
+++ app/models/device.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey
 from app.database import Base
+from sqlalchemy.orm import relationship
 
 class Device(Base):
     __tablename__ = "devices"
@@ -9,4 +10,10 @@ class Device(Base):
     device_type = Column(String, nullable=False)
     serial_number = Column(String, unique=True, nullable=False)
     status = Column(Enum("available", "assigned", "damaged", name="status_enum"), default="available")
-    location = Column(String)
+    location_id = Column(Integer, ForeignKey("locations.id"))
+
+
+    #Realationships
+    location = relationship("Location", back_populates="devices")
+    assigmnents = relationship("Assignment", back_populates="devices")
+    maintenance_logs = relationship("Maintenance", back_populations="devices")
